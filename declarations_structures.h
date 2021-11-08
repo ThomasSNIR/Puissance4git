0937c30678235698b565b89dbc0bfdf4d03d3782
ThomasSNIR <tomacaron37@gmail.com>
2021-11-08 17:21:42 +0100

    nouveau départ

--- declarations_structures.h
+++ declarations_structures.h
@@ -8,13 +8,15 @@
 *************************************************************/
 #ifndef DECLARATIONS_STRUCTURES_H
 #define DECLARATIONS_STRUCTURES_H
-typedef struct // Declarations de 2 structures, 1 pour chaque joueur qui renferme la colonne que le joueur choisi et la "couleur" du joueur
+typedef struct // Declarations de structure joueur qui renferme la colonne que le joueur choisi et la "couleur" du joueur
 {
     char nom[10];
     int couleur;
     int colonne;
 } Joueur ;
-typedef struct
+
+
+typedef struct // declarations structure de la grille
 {
     char Grille[7][6];
 }Grille;
